868eec0c6e7e774142085aded0cf39caa98e1813
asela1982 <asela.d82@gmail.com>
2017-12-23 23:11:42 -0600

    version 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='edaplotting',
-      version='0.3',
+      version='0.4',
       description='plotting for exploratory data analysis',
       url='http://github.com/asela1982/edaplotting',
       author='Asela Dassanayake',
